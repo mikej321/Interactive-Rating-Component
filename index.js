3af166ae99daa52310af210160c3410e78a18da8
mikej321 <111183140+mikej321@users.noreply.github.com>
2022-12-04 10:51:00 -0500

    CSS complete
    An interactive rating component for frontend mentor. I finished the styling and am currently working on the JS for this site.

---
+++ index.js
@@ -0,0 +1,18 @@
+// Create Variables for the project
+
+// Hide the 'Thank you' form until the submit button is clicked
+
+// grab all of the values from the buttons
+
+// listen for clicks on the buttons
+
+// output the clicked value into the thanks form
+const mainForm = document.querySelector('.main-form');
+const thanks = document.querySelector('.thanks');
+const buttons = document.querySelector('.choice-button');
+const submit = document.querySelector('.submit-button');
+const selectedValue = document.querySelector('.selected-value');
+
+let value;
+
+thanks.remove();
